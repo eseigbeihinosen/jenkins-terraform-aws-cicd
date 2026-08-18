e700a7ef09efa1cdfc12faaa9bea3dcf0f08ce19
eseigbeihinosen <eseigbeihinosen@gmail.com>
2026-08-18 23:19:40 +0100

    Update main.tf

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -4,11 +4,11 @@ resource "aws_security_group" "this" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "Jenkins"
+    description = "Jenkins Webhook"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = [var.jenkins_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
